450ea1c8008033e72cc67c4c822ab2e14dde91c4
tha <Sankavi.Thayananthan@ti8m.ch>
2021-12-09 12:54:27 +0100

    ChangeReminder View done

---
+++ app/src/main/java/ch/tha/picturereminder/changeReminder.java
@@ -0,0 +1,14 @@
+package ch.tha.picturereminder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class changeReminder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_reminder);
+    }
+}
