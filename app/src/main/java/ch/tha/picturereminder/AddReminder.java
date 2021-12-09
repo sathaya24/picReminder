61fb593efe416f6cc08606a57d5d79f2e3b1efbd
tha <Sankavi.Thayananthan@ti8m.ch>
2021-12-09 11:26:55 +0100

    AddReminderView done

---
+++ app/src/main/java/ch/tha/picturereminder/AddReminder.java
@@ -0,0 +1,14 @@
+package ch.tha.picturereminder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddReminder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_reminder);
+    }
+}
