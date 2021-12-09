dc12bb63b98f46d44bbd31267314bdab0e554c09
tha <Sankavi.Thayananthan@ti8m.ch>
2021-12-09 13:46:25 +0100

    Datepickerdialog pop up done

--- app/src/main/java/ch/tha/picturereminder/AddReminder.java
+++ app/src/main/java/ch/tha/picturereminder/AddReminder.java
@@ -1,22 +1,60 @@
 package ch.tha.picturereminder;
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Calendar;
+
 public class AddReminder extends AppCompatActivity {
-    private EditText title;
-    private EditText date;
-    private EditText time;
+    private EditText titleTxt;
+    private EditText dateTxt;
+    private EditText timeTxt;
+    private DatePickerDialog datePickerDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_reminder);
 
-        title = (EditText) findViewById(R.id.nameRe);
-        date = (EditText) findViewById(R.id.dateRe);
-        time = (EditText) findViewById(R.id.timeRe);
+        titleTxt = (EditText) findViewById(R.id.nameRe);
+        dateTxt = (EditText) findViewById(R.id.dateRe);
+        timeTxt = (EditText) findViewById(R.id.timeRe);
+        Button cancelBtn = findViewById(R.id.cancelBtnAdd);
+
+        dateTxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar calendar = Calendar.getInstance();
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+                int month = calendar.get(Calendar.MONTH);
+                int year = calendar.get(Calendar.YEAR);
+
+                datePickerDialog = new DatePickerDialog(AddReminder.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        dateTxt.setText(dayOfMonth + "/" + (month + 1) + "/" + year);
+                    }
+                }, year, month, day);
+                datePickerDialog.show();
+            }
+        });
+
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                titleTxt.setText("");
+                timeTxt.setText("");
+                dateTxt.setText("");
+                startActivity(intent);
+            }
+        });
     }
 }
