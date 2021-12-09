61c7f4a68ce54c04730a53dfaa0f27503b8f4f5f
tha <Sankavi.Thayananthan@ti8m.ch>
2021-12-09 13:17:07 +0100

    Change MainView to AddReminderView

--- app/src/main/java/ch/tha/picturereminder/AddReminder.java
+++ app/src/main/java/ch/tha/picturereminder/AddReminder.java
@@ -1,14 +1,22 @@
 package ch.tha.picturereminder;
 
+import android.os.Bundle;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class AddReminder extends AppCompatActivity {
+    private EditText title;
+    private EditText date;
+    private EditText time;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_reminder);
+
+        title = (EditText) findViewById(R.id.nameRe);
+        date = (EditText) findViewById(R.id.dateRe);
+        time = (EditText) findViewById(R.id.timeRe);
     }
 }
--- app/src/main/java/ch/tha/picturereminder/MainActivity.java
+++ app/src/main/java/ch/tha/picturereminder/MainActivity.java
@@ -2,7 +2,10 @@ package ch.tha.picturereminder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button addBtn = findViewById(R.id.addBtn);
+
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), AddReminder.class);
+                startActivity(intent);
+            }
+        });
     }
 }
